3a31d81c5d2444e02f85306f207a5de3acadfeb3
bohdannikoletti <bohdanrose1@gmail.com>
2017-05-31 17:32:02 +0300

    Big changes came
    Working beta 1

--- app/src/main/java/ua/com/geekowl/yodatalk/MainActivity.kt
+++ app/src/main/java/ua/com/geekowl/yodatalk/MainActivity.kt
@@ -1,5 +1,7 @@
 package ua.com.geekowl.yodatalk
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,30 +11,62 @@ import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.getAs
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.NetworkInfo
+import android.widget.ShareActionProvider
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     val LOG_TAG = "YODA TALK APP"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // adding base and header to request
         FuelManager.instance.basePath = "https://yoda.p.mashape.com/yoda?sentence="
-        FuelManager.instance.baseHeaders = mapOf("X-Mashape-Key" to getString(R.string.YODA_SPEACH_TOKEN),
+        FuelManager.instance.baseHeaders = mapOf(
+                "X-Mashape-Key" to getString(R.string.YODA_SPEACH_TOKEN),
                 "Accept" to "text/plain")
         sendButton.setOnClickListener {
-            val userSentence = sentenceTextView.text
-            if(userSentence.length < 5){
+            if (!isConnected()) {
+                Toast.makeText(applicationContext, R.string.network_unavailable,
+                        Toast.LENGTH_LONG).show()
+            } else {
+                val userSentence = sentenceTextView.text
+                if (!inputIsValid("$userSentence")) {
+                    Toast.makeText(applicationContext, R.string.wrong_input,
+                            Toast.LENGTH_LONG).show()
+                } else {
+                    Fuel.get("$userSentence").responseString { request, response, result ->
+                        result.fold({ serverData ->
+                            generatedSentence.text = serverData
+                            // Log.v(LOG_TAG, serverData.toString())
+                        }, { err ->
+                            Toast.makeText(applicationContext,  "Response with error ${err.message}",
+                                    Toast.LENGTH_LONG).show()
+                            Log.e(LOG_TAG, err.message)
+                        })
+                    }
+                }
+            }
 
-            }
-            Fuel.get("$userSentence").responseString { request, response, result ->
-                //do something with response
-                result.fold({ serverData ->
-                    //do something with data
-                    Log.v(LOG_TAG, serverData.toString())
-                }, { err ->
-                    //do something with error
-                    Log.e(LOG_TAG, err.message)
-                })
-            }
         }
+    }
+    // Checking network access
+    private fun isConnected(): Boolean {
+        val cm = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        if(activeNetwork != null){
+            return activeNetwork.isConnectedOrConnecting
+        } else {
+            return false
+        }
+    }
+    private fun inputIsValid(text: String): Boolean{
+        val regEx  = Regex("^[a-zA-Z ]+$")
+        if(regEx.matchEntire(text) ==  null){ return false; }
+        Log.d(LOG_TAG,text.split("\\s+").size.toString())// TODO check functionality
+        return true;
     }
 }
